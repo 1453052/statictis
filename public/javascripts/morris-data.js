fc09056b909fdf0993a6e9adc98b8ccc72bc7356
1453025 <1453025@student.hcmus.edu.vn>
2017-04-11 00:12:52 +0700

    complete 3 chart

--- public/javascripts/morris-data.js
+++ public/javascripts/morris-data.js
@@ -1,7 +1,7 @@
 $(function () {
     Morris.Area({
         element: 'morris-area-chart',
-        data: data,
+        data: data_area,
         xkey: 'period',
         ykeys: ['SachThieuNhi', 'SachGiaoKhoa', 'SachKhoaHoc'],
         labels: ['Sach Thieu Nhi', 'Sach Giao Khoa', 'Sach Khoa Hoc'],
@@ -12,42 +12,7 @@ $(function () {
 
     Morris.Bar({
         element: 'morris-bar-chart',
-        data: [{
-            y: '2006',
-            SachThieuNhi: 2687,
-            SachGiaoKhoa: 4260,
-            SachKhoaHoc: 2428
-        }, {
-            y: '2007',
-            SachThieuNhi: 1687,
-            SachGiaoKhoa: 4260,
-            SachKhoaHoc: 2018
-        }, {
-            y: '2008',
-            SachThieuNhi: 2687,
-            SachGiaoKhoa: 5460,
-            SachKhoaHoc: 6328
-        }, {
-            y: '2009',
-            SachThieuNhi: 3687,
-            SachGiaoKhoa: 3460,
-            SachKhoaHoc: 6028
-        }, {
-            y: '2010',
-            SachThieuNhi: 3687,
-            SachGiaoKhoa: 3460,
-            SachKhoaHoc: 6028
-        }, {
-            y: '2011',
-            SachThieuNhi: 4687,
-            SachGiaoKhoa: 3460,
-            SachKhoaHoc: 3628
-        }, {
-            y: '2012',
-            SachThieuNhi: 5687,
-            SachGiaoKhoa: 6460,
-            SachKhoaHoc: 3228
-        }],
+        data: data_bar,
         xkey: 'y',
         ykeys: ['SachThieuNhi', 'SachGiaoKhoa', 'SachKhoaHoc'],
         labels: ['SachThieuNhi', 'SachGiaoKhoa', 'SachKhoaHoc'],
@@ -57,22 +22,7 @@ $(function () {
 
     Morris.Donut({
         element: 'morris-donut-chart',
-        data: [{
-            label: "Chi Nhánh A",
-            value: 2320
-        }, {
-            label: "Chi Nhánh B",
-            value: 3520
-        }, {
-            label: "Chi Nhánh C",
-            value: 2120
-        }, {
-            label: "Chi Nhánh D",
-            value: 2210
-        }, {
-            label: "Chi Nhánh E",
-            value: 2210
-        }],
+        data: data_donut,
         resize: true
     });
 });
--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-
-    var collection = db.get().collection('doanh_so');
-    collection.find({}, {
+    var collection_doanh_so = db.get().collection('doanh_so');
+    var collection_doanh_thu_theo_the_loai = db.get().collection('doanh_thu_theo_the_loai');
+    var collection_doanh_thu_theo_chi_nhanh = db.get().collection('doanh_thu_theo_chi_nhanh');
+    collection_doanh_so.find({}, {
         period: 1,
         SachThieuNhi: 1,
         SachGiaoKhoa: 1,
@@ -14,11 +15,35 @@ router.get('/', function (req, res, next) {
         _id: 0
     }).toArray(function (err, data) {
         if (err) {
-            console.log('Loi query' + err);
+            console.log('Loi query bang doanh_so' + err);
         }
-        console.log(data);
-        res.render('index', {title: 'Express', data: data});
+        data_area = data;
+    });
+    collection_doanh_thu_theo_the_loai.find({}, {
+        y: 1,
+        SachThieuNhi: 1,
+        SachGiaoKhoa: 1,
+        SachKhoaHoc: 1,
+        _id: 0
+    }).toArray(function (err, data) {
+        if (err) {
+            console.log('Loi query bang doanh_thu_theo_the_loai' + err);
+        }
+        data_bar = data;
     })
+
+    collection_doanh_thu_theo_chi_nhanh.find({}, {
+        label: 1,
+        value: 1,
+        _id: 0
+    }).toArray(function (err, data) {
+        if (err) {
+            console.log('Loi query bang doanh_thu_theo_chi_nhanh' + err);
+        }
+        data_donut = data;
+    })
+    console.log(data_area)
+    res.render('index', {title: 'Express', data_area: data_area, data_bar: data_bar, data_donut: data_donut});
 });
 
 module.exports = router;
